24d123ebfa7cfe42bb1d069056cf964f4371b1fd
Nikolas Zec <nikolas.zec@gmail.com>
2020-06-17 13:00:13 +0200

    stripe backend and fronted implemetation  completed

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51Gsq40K6oIvCzhF3Rg6tt2sBTmLMEguJAHvBYRQVgMbTUMXIgOo1QWHdo7NgTw5SGeTsthPSLaLhmMqhNnhRWKAc00k655pkzs";
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment succesful");
+    axios({
+      url: "/payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((res) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error:", error);
+        alert(
+          "There was issue with your payment. Please use provided credit card."
+        );
+      });
   };
   return (
     <StripeCheckout
