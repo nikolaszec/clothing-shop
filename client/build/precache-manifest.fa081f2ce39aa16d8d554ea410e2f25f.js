6337a417cda7df578754cdc8d913f441857bec0d
Nikolas Zec <nikolas.zec@gmail.com>
2020-06-17 15:23:23 +0200

    adding client depenedecies to root pack.json

---
+++ client/build/precache-manifest.fa081f2ce39aa16d8d554ea410e2f25f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790648dab32b6137c042bc40bfb715b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04d8f0c3be508878d267",
+    "url": "/static/css/main.91685a39.chunk.css"
+  },
+  {
+    "revision": "ba048789cbee555ef879",
+    "url": "/static/js/2.a9a05153.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.a9a05153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d8f0c3be508878d267",
+    "url": "/static/js/main.0e8e8adf.chunk.js"
+  },
+  {
+    "revision": "25540b4bba79d2bfd3c2",
+    "url": "/static/js/runtime-main.2f917a21.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/logo.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
